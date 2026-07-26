8fe46e4141224d56811be56620be02d92b23c189
Suvaly Dey <suvalylt@gmail.com>
2026-07-26 23:45:32 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==1) return 0;
+        int max=prices[prices.length-1];
+        int maxp=0;
+        for(int i=prices.length-1; i>=0; i--)
+        {
+            if(prices[i]>max) max=prices[i];
+            else
+            {
+                maxp=Math.max(maxp,max-prices[i]);
+            }
+        }
+        return maxp;
+    }
+}
